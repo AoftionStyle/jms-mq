702d610aff8ae9193871e62da4ca87ab8c2f6c9a
AOFTION <aoftion@AOFTIONs-MacBook-Air.local>
2022-02-17 15:21:12 +0700

    initial jms-mq

---
+++ src/main/java/co/aofitonstyle/spring/boot/jmsmq/JmsMqApplication.java
@@ -0,0 +1,13 @@
+package co.aofitonstyle.spring.boot.jmsmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmsMqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/aofitonstyle/spring/boot/jmsmq/JmsMqApplicationTests.java
@@ -0,0 +1,13 @@
+package co.aofitonstyle.spring.boot.jmsmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
