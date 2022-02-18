43744f6025466f6c83054e67aa84bca6fead92c1
Chutipon Pongpanit <chutipon.ppn@gmail.com>
2022-02-18 12:21:05 +0700

    replaced test

---
+++ src/test/java/co/aoftionstyle/spring/boot/jms/mq/JmsMQApplicationTests.java
@@ -0,0 +1,13 @@
+package co.aoftionstyle.spring.boot.jms.mq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsMQApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
